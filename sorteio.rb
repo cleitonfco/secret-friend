db48f9005eb098d5ae71c9cd24de98ebdc770682
Cleiton Francisco <cleitonfco@gmail.com>
2015-11-24 18:50:08 -0300

    Versão 2015.alpha - TESTE - apenas exibe os dados no terminal, não envia e-mails

--- sorteio.rb
+++ sorteio.rb
@@ -2,14 +2,25 @@
 require './lib/raffle'
 
 friends = [
-  Person.new('Nome Participante', 'email@participante.com'), 
-  Person.new('Nome Participante', 'email@participante.com'), 
-  Person.new('Nome Participante', 'email@participante.com'), 
-  Person.new('Nome Participante', 'email@participante.com'), 
-  Person.new('Nome Participante', 'email@participante.com'), 
-  Person.new('Nome Participante', 'email@participante.com')
+  Person.new('Alysson Daniel', 'alysson@jus.com.br'), 
+  Person.new('Ayla Maria', 'ayla@jus.com.br'), 
+  Person.new('Cleiton Francisco', 'cleiton@jus.com.br'), 
+  Person.new('Elinaldo Nascimento', 'elinaldo@jus.com.br'), 
+  Person.new('Mateus Pontes', 'mateus@jus.com.br'), 
+  Person.new('Paulo Gustavo', 'paulo@jus.com.br'), 
+  Person.new('Adenisia', 'adenisia@jus.com.br'), 
+  Person.new('Rodrigo', 'rodrigo@jus.com.br'), 
+  Person.new('Carvalho', 'carvalho@jus.com.br'), 
+  Person.new('Italo', 'italo@jus.com.br'), 
+  Person.new('Igor', 'igor@jus.com.br')
 ]
 
-r = Raffle.new(friends)
-r.mix
-r.send
+10.times do |x|
+	puts "=== Sorteio Nº #{x} ==="
+	puts "-----------------------"
+	r = Raffle.new(friends)
+	r.mix
+	r.send
+	puts "+++++++++++++++++++++++"
+	puts ""
+end
