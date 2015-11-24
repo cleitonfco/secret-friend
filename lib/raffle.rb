5d8184fcadcd39b13cfef47815d56de922f69a35
Cleiton Francisco <cleitonfco@gmail.com>
2015-11-24 18:50:08 -0300

    Versão 2015.alpha - TESTE - apenas exibe os dados no terminal, não envia e-mails

--- lib/raffle.rb
+++ lib/raffle.rb
@@ -13,33 +13,52 @@ class Person
   def to_s
     "#{@name}: #{@email}"
   end
+
+  def select(raffle)
+    raffle.knuth_shuffle
+    friend = valids.shift
+    valids = raffle.shuffle - [self]
+    raffle.shuffle -= [friend]
+    raise 'Conflito no Sorteio. Não foi possível selecionar um amigo.' unless friend
+    self.friend = friend.name
+    self
+  end
 end
 
 class Raffle
-  attr_accessor :participants
+  attr_accessor :participants, :shuffle
 
   def initialize(participants = [])
-    @participants = participants
-    @shuffle = @participants
-    @raffleds = []
     @mix = nil
-    rand(100).times { @shuffle = @shuffle.shuffle }
+    @participants = participants
+    reload_shuffle
   end
 
-  def select(person)
-    friend = @shuffle.shuffle!.shift
-    if friend == person
-      @shuffle.push(person)
-      puts "***** CONFLITO: #{friend} = #{person}. Buscando outro amigo! *****"
-      return select(person)
+  def reload_shuffle
+    @shuffle = @participants.dup
+    knuth_shuffle
+  end
+
+  def knuth_shuffle
+    j = @shuffle.length
+    i = 0
+    while j > 1
+      r = i + rand(j)
+      @shuffle[i], @shuffle[r] = @shuffle[r], @shuffle[i]
+      i += 1
+      j -= 1
     end
-    person.friend = friend.name
-    person
+    @shuffle
   end
 
   def mix
     return @mix if @mix && !@mix.empty?
-    @mix = @participants.map { |p| select(p) }
+    begin
+      @mix = @participants.map { |p| p.select(self) }
+    rescue
+      reload_shuffle
+      mix
+    end
   end
 
   def send
@@ -49,39 +68,41 @@ class Raffle
       config[:email] = p.email
       config[:name] = p.name
       config[:friend] = p.friend
-      prepare_mail(config) if mix.size == @participants.size
+      # prepare_mail(config) if mix.size == @participants.size
+      puts "Sorteio: #{p.name} => #{p.friend}"
     end
   end
 
-  protected
-    def prepare_mail(config)
-      mail = Mail.new do
-        content_type 'text/plain; charset=UTF-8'
-        from         'email@gmail.com'   # Configure aqui o e-mail do remetente
-        to           config[:email]
-        subject      'Amigo Oculto 2014'
-        body         <<-EOF
-Oi #{config[:name]},
+#   protected
+#     def prepare_mail(config)
+#       mail = Mail.new do
+#         content_type 'text/plain; charset=UTF-8'
+#         from         'naoresponder@jus.com.br'   # Configure aqui o e-mail do remetente
+#         to           config[:email]
+#         bcc          'cleitonfco@gmail.com'
+#         subject      'Amiga Secreta 2014'
+#         body         <<-EOF
+# Oi #{config[:name]},
 
-Você está participando do AMIGO OCULTO 2014 e seu Amigo é: #{config[:friend]}
+# Você está participando da AMIGA SECRETA DE 2014 e sua Amiga é a #{config[:friend]}
 
-Boas Festas.
------------------------
-Esta mensagem foi enviada automaticamente, portanto o sigilo dela só depende de você. :-)}
-EOF
-      end
+# Boas Festas.
+# -----------------------
+# Esta mensagem foi enviada automaticamente, portanto o sigilo dela só depende de você. :-)}
+# EOF
+#       end
 
-      mail.deliver!
-    end
+#       mail.deliver!
+#     end
 end
 
-Mail.defaults do
-  delivery_method :smtp, { 
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :user_name => "email@gmail.com",  # Configure aqui o e-mail de login
-    :password => "***********",       # Configure aqui a senha do usuario acima
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-end
+# Mail.defaults do
+#   delivery_method :smtp, { 
+#     :address => 'smtp.gmail.com',
+#     :port => 587,
+#     :user_name => "naoresponder@jus.com.br",
+#     :password => "m2E9lXi9s7s8A",
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
+#   }
+# end
