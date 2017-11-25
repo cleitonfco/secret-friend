4f2c0b7d1bbf8184950e777bc6a29c9cd0448c6f
Cleiton Francisco <cleitonfco@gmail.com>
2017-11-25 16:33:16 -0300

    Pessoas

--- sorteio.rb
+++ sorteio.rb
@@ -3,16 +3,14 @@ require './lib/person'
 require './lib/raffle'
 
 friends = [
-  Person.new('Paulo Gustavo',       'paulog@gmail.com'), 
-  Person.new('Ayla Maria',          'ayla.rocha@gmail.com'), 
-  Person.new('Cleiton Francisco',   'cleitonfco@gmail.com'), 
-  Person.new('Adenisia Lima',       'adenisialima@hotmail.com'), 
-  Person.new('Rodrigo Chaves',      'rodrigo@jus.com.br'), 
-  Person.new('Alysson Daniel',      'alyssondaniel@gmail.com'), 
-  Person.new('Mateus Pontes',       'mateuspo10@gmail.com'), 
-  Person.new('Elinaldo Nascimento', 'elinaldo.java@gmail.com'), 
-  Person.new('José Carvalho',       'carvalho@jus.com.br'), 
-  Person.new('Italo Carvalho',      'italo@jus.com.br'), 
+  Person.new('José Carvalho',        'carvalho@jus.com.br'), 
+  Person.new('Elinaldo Nascimento',  'elinaldo.java@gmail.com'), 
+  Person.new('Ayla Maria',           'ayla.rocha@gmail.com'), 
+  Person.new('Adenisia Lima',        'adenisialima@hotmail.com'), 
+  Person.new('Rodrigo Chaves',       'rodrigo@jus.com.br'), 
+  Person.new('Wellington (Sorriso)', 'sorriso@jus.com.br'), 
+  Person.new('Cleiton Francisco',    'cleitonfco@gmail.com'), 
+  Person.new('Paulo Gustavo',        'paulog@gmail.com'), 
 ]
 
 r = Raffle.new(friends)
